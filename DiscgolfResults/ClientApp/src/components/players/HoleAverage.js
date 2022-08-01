5b258c0a76c3844bf79439cffe1f80c063173c5f
erimo832@gmail.com <erimo832@gmail.com>
2022-08-01 09:40:31 +0200

    Refactor move out fetch data from hole avg to player datails component

--- DiscgolfResults/ClientApp/src/components/players/HoleAverage.js
+++ DiscgolfResults/ClientApp/src/components/players/HoleAverage.js
@@ -1,116 +1,46 @@
-import { Slider, Button, CircularProgress } from '@mui/material';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import { Box } from '@mui/system';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 export function HoleAverage(props) {
   const { i18n } = useTranslation();
 
-  const [loading, setLoading] = useState(true);  
-  const [holeAvg, setHoleAvg] = useState(null);
-  const [events, setEvents] = useState(null);
-
-  ////////Slider
-  const [fromEvent, setFromEvent] = useState(-1);
-  const [toEvent, setToEvent] = useState(-1);
-  const [value, setValue] = useState([1, 1]);
-
-
-  const handleChange = (event, value) => {
-    if(value.length === 2) {
-      setFromEvent(events[value[0] - 1].eventId);
-      setToEvent(events[value[1] - 1].eventId);
-    }
-
-    setValue(value);
-  };
-
-  function valueLabelFormat(value) {
-    return events[value - 1].eventName + " (" + events[value - 1].startTime.substring(0,10) + ")";
-  }
-
-  /////////
-
-  let contents = loading
-      ? <Box display="flex" justifyContent="center" alignItems="center"><CircularProgress /></Box>
-      : getContent(holeAvg);
-
-  useEffect(() => {
-    if(holeAvg === null)
-      fetchData(true);
-  });
-
   function getContent(data) {
     if(data.length === 0)
         return (<div>{i18n.t('playersinfo_no_found')}</div>);
-
+    
     return (
-      <>      
-      <Button variant="outlined" startIcon={<RefreshIcon />} onClick={() => { fetchData(false); }}>
-        {i18n.t('common_refresh')}
-      </Button>
-      <Box display="flex" flexDirection="column" ml={16} mr={16} mt={2} mb={2}>
-        <Slider
-          value={value}
-          min={1}
-          step={1}
-          max={events.length}          
-          getAriaValueText={valueLabelFormat}
-          valueLabelFormat={valueLabelFormat}
-          onChange={handleChange}
-          valueLabelDisplay="auto"
-          aria-labelledby="non-linear-slider"
-        />
-      </Box>
-      <ResponsiveContainer width="100%" height="100%" minHeight={300}>
-        <BarChart width={500} height={300} data={data} margin={{ top: 5, right: 5, left: 5, bottom: 5, }} >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="holeNumber" />
-          <YAxis domain={[2, 'auto']} />
-          <Tooltip content={<HoleAvgTooltip />}  />
-          <Legend />
-          <Bar name={i18n.t('hole_stats_avgscore')} dataKey="averageScore" fill="#8884d8" />
-          <Bar name={i18n.t('hole_stats_par')} dataKey="par" fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
-      <ResponsiveContainer width="100%" height="100%" minHeight={300}>
-        <BarChart width={500} height={300} data={data} margin={{ top: 5, right: 5, left: 5, bottom: 5, }} >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="holeNumber" />
-          <YAxis domain={['auto', 'auto']} />
-          <Tooltip content={<HoleAvgTooltip />}  />
-          <Legend />
-          <Bar name={i18n.t('hole_stats_diffpar')} dataKey="diffToPar" fill="#8884d8" />
-        </BarChart>
-      </ResponsiveContainer>
+      <> 
+        <ResponsiveContainer width="100%" height="100%" minHeight={300}>
+          <BarChart width={500} height={300} data={data} margin={{ top: 5, right: 5, left: 5, bottom: 5, }} >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="holeNumber" />
+            <YAxis domain={['auto', 'auto']} />
+            <Tooltip content={<HoleAvgTooltip />}  />
+            <Legend />
+            <Bar name={i18n.t('hole_stats_diffpar')} dataKey="diffToPar" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
+        <ResponsiveContainer width="100%" height="100%" minHeight={300}>
+          <BarChart width={500} height={300} data={data} margin={{ top: 5, right: 5, left: 5, bottom: 5, }} >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="holeNumber" />
+            <YAxis domain={[2, 'auto']} />
+            <Tooltip content={<HoleAvgTooltip />}  />
+            <Legend />
+            <Bar name={i18n.t('hole_stats_avgscore')} dataKey="averageScore" fill="#8884d8" />
+            <Bar name={i18n.t('hole_stats_par')} dataKey="par" fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
       </>
     );
   }
-  async function fetchData(isInit) {
-    setLoading(true);
 
-    let path = '/api/players/'+ props.playerId + '/average-hole-results?fromEventId=' + fromEvent + '&toEventId=' + toEvent;
-    const response = await fetch(path);
-    const data = await response.json();
-
-    let eventPath = '/api/players/'+ props.playerId + '/events';
-    const responseEvents = await fetch(eventPath);
-    const dataEvents = await responseEvents.json();
-
-    if(isInit)
-      setValue([1, dataEvents.length]);
-
-    setHoleAvg(data);
-    setEvents(dataEvents);
-    setLoading(false);
-  };
   return (
-    <div>
-        {contents}
-    </div>
+    <>
+      {getContent(props.data)}
+    </>
   );
 }
 
--- DiscgolfResults/ClientApp/src/components/players/PlayerDetails.js
+++ DiscgolfResults/ClientApp/src/components/players/PlayerDetails.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import RefreshIcon from '@mui/icons-material/Refresh';
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import Collapse, { Panel } from 'rc-collapse';
 import { HoleAverage } from './HoleAverage';
-import { Box, CircularProgress } from '@mui/material';
+import { Box, Button, CircularProgress, Slider } from '@mui/material';
 import { SummaryCard } from './SummaryCard';
 import { ScoreDistribution } from './ScoreDistribution';
 import { HcpTrend } from './HcpTrend';
@@ -13,10 +14,31 @@ import { Events } from './Events';
 
 
 export function PlayerDetails() {
+  const handleChange = (event, value) => {
+    if(value.length === 2) {
+      setFromEvent(events[value[0] - 1].eventId);
+      setToEvent(events[value[1] - 1].eventId);
+    }
+
+    setValue(value);
+  };
+  
   const [loading, setLoading] = useState(true);
   const [rounds, setRounds] = useState(null);
+
+  //HoleAvg
+  const [holeAvg, setHoleAvg] = useState(null);
+
+  //Slider and events
+  const [events, setEvents] = useState(null);
+  const [fromEvent, setFromEvent] = useState(-1);
+  const [toEvent, setToEvent] = useState(-1);
+  const [value, setValue] = useState([1, 1]);
+
   let params = useParams(rounds);
   const { i18n } = useTranslation(); //t
+
+  
 
   let contents = loading
       ? <Box display="flex" justifyContent="center" alignItems="center"><CircularProgress /></Box>
@@ -24,17 +46,30 @@ export function PlayerDetails() {
 
   useEffect(() => {
     if(rounds === null)
-      fetchData();
+      fetchData(true);
   });
 
+
+  function valueLabelFormat(value) {
+    return events[value - 1].eventName + " (" + events[value - 1].startTime.substring(0,10) + ")";
+  }
+
   function renderDetailsTable(info) {
-    if(info.length === 0)
+    if(info === null)
         return (<div>{i18n.t('playersinfo_no_found')}</div>);
     
     let sortedEvents = info.eventResults.map( (x) => x ).sort( (a, b) => a.playedEvent - b.playedEvent ); //map for simple clone
 
     return (
         <>
+          
+          <Button variant="outlined" startIcon={<RefreshIcon />} onClick={() => { fetchData(false); }}>
+            {i18n.t('common_refresh')}
+          </Button>
+          <Box display="flex" flexDirection="column" ml={16} mr={16} mt={2} mb={2}>
+            <Slider value={value} min={1} step={1} max={events.length} getAriaValueText={valueLabelFormat} valueLabelFormat={valueLabelFormat}
+              onChange={handleChange} valueLabelDisplay="auto" aria-labelledby="non-linear-slider"/>
+          </Box>
           <SummaryCard player={info} />
           <Collapse accordion={false}>
             <Panel header={i18n.t('player_details_hcptrend')}>
@@ -44,7 +79,7 @@ export function PlayerDetails() {
               <AggregatedScoreTrend data={sortedEvents} />
             </Panel>
             <Panel header={i18n.t('player_details_holeavg')}>
-              <HoleAverage playerId={params.playerId} />
+              <HoleAverage data={holeAvg} />
             </Panel>
             <Panel header={i18n.t('player_details_scoredistibution')}>
               <ScoreDistribution data={info.scoreDistibution} />
@@ -57,13 +92,42 @@ export function PlayerDetails() {
     );
   }
   
-  async function fetchData() {
+  async function fetchData(isInit) {
+    
+    await fetchDetails();
+    await fetchHoleAverages();
+
+    if(isInit)
+      await fetchEvents(isInit);
+
+    setLoading(false);
+  };
+
+  async function fetchDetails() {
     let path = '/api/players/'+ params.playerId + '/details';
     const response = await fetch(path);
     const data = await response.json();
 
     setRounds(data);
-    setLoading(false);
+  };
+
+  async function fetchEvents(isInit) {
+    let eventPath = '/api/players/'+ params.playerId + '/events';
+    const responseEvents = await fetch(eventPath);
+    const dataEvents = await responseEvents.json();
+
+    if(isInit)
+      setValue([1, dataEvents.length]);
+
+    setEvents(dataEvents);
+  };
+
+  async function fetchHoleAverages() {
+    let path = '/api/players/'+ params.playerId + '/average-hole-results?fromEventId=' + fromEvent + '&toEventId=' + toEvent;
+    const response = await fetch(path);
+    const data = await response.json();
+
+    setHoleAvg(data);
   };
 
   return (
